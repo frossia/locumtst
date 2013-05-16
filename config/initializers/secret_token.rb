9df7391e7502e151b623f8c7e1219bc631aaa634
frossia <frossiacsb@gmail.com>
2013-05-16 17:06:23 +0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locumtst::Application.config.secret_token = '2a2926fb27bcd8cf6bd5b8cf8169e7da650acea08bb6d147b1c366fa2ed42098f9be4c78a488345d405fa5a81b57d16eaa139a8c379d44ca46e2200c763f70e1'
